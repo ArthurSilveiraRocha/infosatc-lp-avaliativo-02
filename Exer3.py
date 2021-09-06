cc9b4bd55f117884c8897fa4fdd8158521657057
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-09-06 16:15:31 -0300

    Create Exer3.py

---
+++ Exer3.py
@@ -0,0 +1,9 @@
+lista = [1,2,3,4,5,6]
+if 5 in lista:
+    print("O número 5 está na lista\n",lista)
+else:
+    print("O número 5 não está na lista")
+if 8 in lista:
+    print("O número 8 está na lista",lista)
+else:
+    print("O número 8 não está na lista")
