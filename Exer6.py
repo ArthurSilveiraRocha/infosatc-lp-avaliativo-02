fc3b36520cfc4bbcf530c57000c14a4eb2c887bb
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-09-06 16:15:39 -0300

    Create Exer6.py

---
+++ Exer6.py
@@ -0,0 +1,5 @@
+pc_gamer = ["Processador i9 10900k","64Gb RAM DDR6","Gabinete Cooler Master","Placa de Vídeo a GeForce RTX 2080Ti"]
+print(pc_gamer)
+pc_gamer.remove("Placa de Vídeo a GeForce RTX 2080Ti")
+pc_gamer.remove("Gabinete Cooler Master")
+print(pc_gamer)
