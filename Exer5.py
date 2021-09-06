5dbc93a1c93807198cb3a4a1687235f90717fe23
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-09-06 16:15:36 -0300

    Create Exer5.py

---
+++ Exer5.py
@@ -0,0 +1,29 @@
+filmes  = ["Cidade de Deus","Poderoso Chefão"]
+print(filmes)
+jogos   = ["God of War","Hallo 6"]
+print(jogos)
+livros  = ["O menino do pijama listrado","Diário de Annie Frank"]
+print(livros)
+esporte = ["Futebol","Basquete"]
+print(esporte)
+filmes.append("John Wick 3: Parabellum")
+filmes.insert(-3, "Doutor Estranho")
+print(filmes)
+jogos.append("Uncharted 2")
+jogos.insert(-2, "Forza Horizon 2")
+print(jogos)
+livros.append("1984")
+livros.insert(0, "A Revolução dos Bichos")
+print(livros)
+esporte.append("Volei")
+esporte.insert(-3, "Futebol Americano")
+print(esporte)
+lista = [filmes,jogos,livros,esporte]
+print(lista)
+print(livros[0])
+esporte.remove("Volei")
+print(esporte)
+disciplina = ["Geografia","Matemática"]
+print(disciplina)
+lista.append(disciplina)
+print(lista)
